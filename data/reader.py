767163aa1f83f225eca76d9c88ab60c92ad0053b
Hassan Algoz <hassanalgoz@gmail.com>
2023-06-12 16:44:14 +0300

    pad and use LongTensor
    - pad because inputs have to be the same length
    - using LongTensor is efficient for index values

--- data/reader.py
+++ data/reader.py
@@ -1,29 +1,28 @@
 import csv
 import os
 
+import torch
 from torch.utils import data
 
 
 class Dataset(data.Dataset):
-    def __init__(self, split, data_dir, embeddings_kv):
-        split_map = {"train": "train.csv", "val": "val.csv", "test": "test.csv"}
-        filename = split_map[split]
-
-        self.xs = []
-        self.ys = []
-
-        with open(os.path.join(data_dir, filename)) as f:
+    def __init__(self, split, data_dir, embeddings_kv, length):
+        xs = []
+        ys = []
+        with open(os.path.join(data_dir, split, "spam.csv")) as f:
             reader = csv.reader(f)
             for row in reader:
-                self.xs.append(
-                    [
-                        embeddings_kv.key_to_index[w]
-                        for w in row[1].split(" ")
-                        if w in embeddings_kv
-                    ]
-                )
-                self.ys.append(int(row[0] == "spam"))
-
+                ws = [
+                    embeddings_kv.key_to_index[w]
+                    for w in row[1].split(" ")
+                    if w in embeddings_kv
+                ]
+                # pad with dots (must pad for input to be accepted; since inputs must be of same size)
+                ws += [embeddings_kv.key_to_index['.']] * (length - len(ws))
+                xs.append(ws[:length])
+                ys.append(int(row[0] == "spam"))
+        self.xs = torch.LongTensor(xs)
+        self.ys = torch.LongTensor(ys)
     def __len__(self):
         return len(self.xs)
 
